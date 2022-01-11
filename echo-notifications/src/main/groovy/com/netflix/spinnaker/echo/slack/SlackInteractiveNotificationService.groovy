745efd82c0d3b4cfd400c0d82044b04d732fdf53
singh09iet <anoop.singh@opsmx.io>
2022-01-11 09:04:56 +0530

    Added a new method for slack info.

--- echo-notifications/src/main/groovy/com/netflix/spinnaker/echo/slack/SlackInteractiveNotificationService.groovy
+++ echo-notifications/src/main/groovy/com/netflix/spinnaker/echo/slack/SlackInteractiveNotificationService.groovy
@@ -119,7 +119,8 @@ class SlackInteractiveNotificationService extends SlackNotificationService imple
     log.info("Slack user payload ${payload.user}")
     String user = payload.user.name
     try {
-      SlackService.SlackUserInfo userInfo = slackAppService.getUserInfo(payload.user.id)
+      log.info("user id ${payload.user.id}")
+      SlackService.SlackUserInfo userInfo = slackAppService.getUserInfo(payload.user.id, false)
       log.info("user info :$userInfo.name")
       log.info("user info :${userInfo.getEmail()}")
       user = userInfo.email
